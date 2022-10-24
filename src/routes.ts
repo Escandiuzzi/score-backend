3cc7bc9d9f786002aa0af72fac6b8e3fc8bcf4cb
Luiz Felipe Escandiuzzi <lfescandiuzzi@icloud.com>
2022-10-24 18:09:48 -0300

    Fix cors bug

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,10 @@
 import cors from 'cors';
 import { Router, Request, Response } from 'express';
-import { StatsReviewer } from './stats-review';
+import { StatsReviewer } from './stats-reviewer';
 
 export const route = Router();
 
 const stats_reviewer = new StatsReviewer();
-
 
 route.get('/', cors(), (req: Request, res: Response) => {
     res.send('Hello World!')
@@ -13,6 +12,6 @@ route.get('/', cors(), (req: Request, res: Response) => {
 
 route.post("/stats", cors(), (req: Request, res: Response) => {    
     const result = stats_reviewer.review(req.body)
-
+    console.log('Reviewer Result', result);
     res.json(result)
 });
